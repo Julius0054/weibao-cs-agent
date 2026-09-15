0748ad4101bdfd8bb8f79a804785df04688bda47
Julius0054 <129638802+Julius0054@users.noreply.github.com>
2026-09-15 20:44:20 +0800

    Add src/main.tsx

---
+++ src/main.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import { APP_CONFIG } from './config';
+import './index.css';
+
+// 设置页面标题
+document.title = APP_CONFIG.name;
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
+);
